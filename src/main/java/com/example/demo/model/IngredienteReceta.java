62f9f7aaec018dc73fdfbd55e36730cfb521d75d
Fernando <fernando_n_g@hotmail.com>
2018-10-17 09:43:52 +0200

    funciona BBDD

--- src/main/java/com/example/demo/model/IngredienteReceta.java
+++ src/main/java/com/example/demo/model/IngredienteReceta.java
@@ -4,12 +4,25 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="ingredientesrecetas")
 public class IngredienteReceta implements Serializable {
 	
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	@Column(name="receta_id")
 	private int receta_id;
 	
