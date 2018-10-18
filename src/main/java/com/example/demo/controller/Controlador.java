7881d1fc0847baee75c6c971b4a75aad5d85a47f
Nacho Villamil <naxovr@gmail.com>
2018-10-18 10:51:04 +0200

    añadido recetaCompleta.jsp y modificacion en Controlador de los mapping

--- src/main/java/com/example/demo/controller/Controlador.java
+++ src/main/java/com/example/demo/controller/Controlador.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.interfaces.ICategoriaService;
+import com.example.demo.interfaces.IRecetaService;
 import com.example.demo.interfaces.IUsuarioService;
+import com.example.demo.model.Receta;
 import com.example.demo.model.Usuario;
 
 @Controller
@@ -19,6 +23,11 @@ public class Controlador {
 	
 	@Autowired
 	private ICategoriaService categoriaService;
+	
+	@Autowired
+	private IRecetaService recetaService;
+	
+	HttpSession session;
 	
 	@RequestMapping("/")
 	public ModelAndView inicio(HttpServletRequest req) {
@@ -55,7 +64,7 @@ public class Controlador {
 	}
 	@RequestMapping("/logear")
 	public ModelAndView login(HttpServletRequest req) {
-		HttpSession session = req.getSession(true);
+		session = req.getSession(true);
 		ModelAndView m = new ModelAndView();
 		String usuario = req.getParameter("usuario");
 		String email = req.getParameter("email");
@@ -73,11 +82,44 @@ public class Controlador {
 
 	@RequestMapping("/categorias")
 	public String categorias (HttpServletRequest req) {
+		session = req.getSession(true);
 		System.out.println("entra en categorias");
-		req.setAttribute("listarCategorias", categoriaService.listarCategorias());
+		session.setAttribute("listarCategorias", categoriaService.listarCategorias());
 		
 		return "categorias";
 	}
+	
+	@RequestMapping("/recetas")
+	public String recetas(HttpServletRequest req) {
+		session = req.getSession(true);
+		System.out.println("entra en recetas");
+
+		List<Receta> listaRecetas = recetaService.listarPorCategoria(3);
+		req.setAttribute("listarRecetas", listaRecetas);
+		
+		// CONFIRMAR QUE ESTA COGIENDO LA ID DE LA RECETA
+//		List<Receta> listaRecetas = recetaService.listarPorCategoria(Integer.parseInt(req.getParameter("id_categoria")));
+//		
+//		req.setAttribute("listarRecetas", listaRecetas);
+		
+		return "recetas";
+	}
+	
+	
+	@RequestMapping("/recetaCompleta")
+	public String recetaCompleta(HttpServletRequest req) {
+		 session = req.getSession(true);
+		System.out.println("entra en recetaCompleta");
+		
+		Receta receta;
+		//receta = recetaService.bus
+		
+		req.setAttribute("listarCategorias", categoriaService.listarCategorias());
+		
+		return "recetaCompleta";
+	}
+	
+	
 	@RequestMapping("/cerrarSesion")
 	public ModelAndView cerrarSesion(HttpServletRequest req) {
 		HttpSession session = req.getSession(true);
--- src/main/webapp/WEB-INF/view/recetas.jsp
+++ src/main/webapp/WEB-INF/view/recetas.jsp
@@ -1,11 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
+
 	pageEncoding="UTF-8"%>
+	<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+	
 <!DOCTYPE html>
 <html>
 <%@ include file="../plantillas/head.jsp"%>
 
 <body>
+	<div class="container-fluid">
 	<%@ include file="../plantillas/header.jsp"%>
+	<section class="container" style="max-width: 25%; float: left;">
+		<ul style="list-style: none;">
+			<c:forEach var="r" items="${listarCategorias}">
+				<li  style="text-align: center; padding: 5%;text-decoration: none;	"><h3><a class="categoria" href="">${r.nombre}</a></h3></li>
+			</c:forEach>
+		</ul>
+	</section>
+	</div>
+	
+	<div style="" class=""></div>
 	
 	</body>
 	</html>
